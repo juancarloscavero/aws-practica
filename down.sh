23f9b45e9936f0b4b3aa3a3bb76678749f1bd9e2
Juan Carlos <juancarlos.cavero@medianet.es>
2016-06-28 12:59:24 +0200

    while for down

--- down.sh
+++ down.sh
@@ -7,3 +7,13 @@ then
 fi
 
 aws ec2 stop-instances --instance-ids $1 --region eu-west-1
+
+status=$(aws ec2 describe-instance-status --instance-id $1 --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
+	while [[ $status != "null" ]]; 
+	do
+		sleep 3
+		echo 'wait'
+		status=$(aws ec2 describe-instance-status --instance-id $1 --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
+	done  
+
+	echo 'Eliminada intancia!'
