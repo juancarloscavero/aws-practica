0f7c7d7ba9b82817747c216f3881934ca3808199
Juan Carlos <juancarlos.cavero@medianet.es>
2016-06-29 12:25:02 +0200

    Finura extrema

--- autoUpDown.sh
+++ autoUpDown.sh
@@ -4,14 +4,14 @@ now=$(date +%H:%M)
 apagar=$(aws ec2 describe-instances --region eu-west-1 --filters 'Name=tag:hora_apagado,Values=*' | jq -r '.Reservations[].Instances[].Tags[] | select(.Key=="hora_apagado")'| jq -r .Value)
 echo $now 
 echo $apagar
-if [ $apagar == $now ]; then
-        id=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=tag:hora_apagado,Values='$hour'" | jq -r .Reservations[].Instances[].InstanceId)
+if [ "$apagar" == "$now" ]; then
+        id=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=tag:hora_apagado,Values='$apagar'" | jq -r .Reservations[].Instances[].InstanceId)
         echo $id
         status=$(aws ec2 describe-instance-status --instance-id $id --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
         echo $status
-        if [ $status != 'stopped' ]; then
+        if [ "$status" != "stopped" ]; then
                 aws ec2 stop-instances --region eu-west-1 --instance-ids $id
-                while [[ $status != "stopped" ]]; 
+                while [[ "$status" != "stopped" ]]; 
 				do
 					sleep 3
 					echo '...'
@@ -22,14 +22,14 @@ if [ $apagar == $now ]; then
         fi
 fi
 encender=$(aws ec2 describe-instances --region eu-west-1 --filters 'Name=tag:hora_encendido,Values=*' | jq -r '.Reservations[].Instances[].Tags[] | select(.Key=="hora_encendido")'| jq -r .Value)
-if [ $encender == $now ]; then
-        id=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=tag:hora_encendido,Values='$hour'" | jq -r .Reservations[].Instances[].InstanceId)
+if [ "$encender" == "$now" ]; then
+        id=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=tag:hora_encendido,Values='$encender'" | jq -r .Reservations[].Instances[].InstanceId)
         echo $id
         status=$(aws ec2 describe-instance-status --instance-id $id --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
         echo $status
-        if [ $status != 'running' ]; then
+        if [ "$status" != "running" ]; then
                 aws ec2 start-instances --region eu-west-1 --instance-ids $id
-                while [[ $status != "running" ]]; 
+                while [[ "$status" != "running" ]]; 
         		do
                 	sleep 3
                 	echo ...
--- up.sh
+++ up.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 var=$(aws ec2  describe-instances --region eu-west-1 --filters 'Name=image-id,Values=ami-ae47dcdd' | jq -r .Reservations[0].OwnerId)
-echo $var
-if [[ $var != null ]]; then
-	
-	id=$(aws ec2 run-instances --image-id ami-ae47dcdd --region eu-west-1 --subnet-id subnet-9b3e9eed --instance-type t2.micro --key-name TESTDEVOPS | jq -r .Instances[].InstanceId) 
-	aws  ec2  create-tags --resources $id --tags Key=Name,Value=Bastion --region eu-west-1
-	sleep 5
-	status=$(aws ec2 describe-instance-status --instance-id $id --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
-	echo $status
-	while [[ $status != "running" ]]; 
-	do
-		sleep 3
-		echo ...
-		status=$(aws ec2 describe-instance-status --instance-id $id --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
-	done  
+if [[ $var == null ]]; then
+
+        id=$(aws ec2 run-instances --image-id ami-ae47dcdd --region eu-west-1 --subnet-id subnet-9b3e9eed --instance-type t2.micro --key-name TESTDEVOPS | jq -r .Instances[].InstanceId)
+        aws  ec2  create-tags --resources $id --tags Key=Name,Value=Bastion --region eu-west-1
+        sleep 5
+        status=$(aws ec2 describe-instance-status --instance-id $id --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
+        while [[ $status != "running" ]]; 
+        do
+                sleep 3
+                echo ...
+                status=$(aws ec2 describe-instance-status --instance-id $id --region eu-west-1 | jq -r .InstanceStatuses[0].InstanceState.Name)
+                echo 'Encendida bitch!!!'
+        done
 
 fi
